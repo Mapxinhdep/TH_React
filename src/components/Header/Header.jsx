e417ab6e4821ad54a15440d9c18dcba585dd33e8
TuanAnh <anhlak0411@gmail.com>
2025-04-09 14:09:18 +0700

    Layout

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,8 @@
+const Header = () => (
+    <header className="flex justify-between items-center p-4 bg-white shadow-sm">
+      <h1 className="text-pink-500 text-3xl font-bold">Dashboard</h1>
+      <input className="border px-4 py-2 rounded-md" placeholder="Search..." />
+    </header>
+  );
+  export default Header;
+  
